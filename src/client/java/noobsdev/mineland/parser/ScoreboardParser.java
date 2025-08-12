47ed84aae311e19640a877f67b0fa53375ca26bf
Shaurmabratiish <volynkinnikita031@gmail.com>
2025-08-12 17:51:51 +0900

    fix

--- src/client/java/noobsdev/mineland/parser/ScoreboardParser.java
+++ src/client/java/noobsdev/mineland/parser/ScoreboardParser.java
@@ -3,6 +3,9 @@ package noobsdev.mineland.parser;
 import noobsdev.mineland.utilities.GsonUtility;
 import noobsdev.mineland.utilities.Player;
 
+import java.sql.Timestamp;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +15,16 @@ public class ScoreboardParser {
         List<String> parseData = new ArrayList<>();
         Player player = new Player();
         parseData.add(GsonUtility.serializeCreativeData("online",player.getCreativeOnline()));
+
+        parseData.add(GsonUtility.serializeCreativeData("last-timestamp", getTimestamp()));
         return parseData;
 
+    }
+    private static String getTimestamp() {
+        ZoneId moscowZone = ZoneId.of("Europe/Moscow");
+        ZonedDateTime moscowTime = ZonedDateTime.now(moscowZone);
+        long unixSeconds = moscowTime.toEpochSecond();
+        String res = "";
+        return res + unixSeconds;
     }
 }
